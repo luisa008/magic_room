e9415297d2b4c5ff30c0ba42c1b6aa7a291a29b0
YungYi0202 <cathy@anyday.com.tw>
2022-05-16 16:20:42 +0800

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let vase;
 // Basic scene
 function initScene(){
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
   camera.position.z = 20;
   camera.position.y = -2;
 
