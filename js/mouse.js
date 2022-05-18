ca9a53a3e981b8f5e89c43f70ef58da7fc986555
YungYi0202 <cathy@anyday.com.tw>
2022-05-18 16:16:08 +0800

    Add mouse control

---
+++ js/mouse.js
@@ -0,0 +1,17 @@
+renderer.domElement.addEventListener("click", onclick, true);
+var raycaster = new THREE.Raycaster();
+function onclick(event) {
+    console.log(event);
+
+    var mouse = new THREE.Vector2();
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  
+    raycaster.setFromCamera(mouse, camera);
+    var intersects = raycaster.intersectObjects(scene.children, true); //array
+    if (intersects.length > 0) {
+        var selectedObject = intersects[0];
+        console.log(selectedObject["object"]);
+        console.log(objList["vase1"]);
+    }
+}
